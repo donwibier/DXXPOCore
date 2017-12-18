40f7b9ed5f08ad0d6866d1c46a2c173e6de40b9d
Don Wibier <donw@MacBook-Don.lan>
2017-12-18 16:55:18 +0100

    xamarin added

---
+++ xamarin/xamarin/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace xamarin
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<XpoDataStore>();
+
+            if (Device.RuntimePlatform == Device.iOS)
+                MainPage = new MainPage();
+            else
+                MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
---
+++ xamarin/xamarin/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace xamarin
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
